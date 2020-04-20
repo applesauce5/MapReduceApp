fccd334e0eca9dc668327192b27ad7e999fb9276
Your Name <you@example.com>
2020-04-20 15:48:53 -0400

    code works with local hadoop file system

---
+++ MapReduceProject/MapReduceAWS/AnalyzeRedditData.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class AnalyzeRedditData {
+
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      //StringTokenizer itr = new StringTokenizer(value.toString());
+      String[] parseText = value.toString().split("[\\r\\n]+");
+      for(String i : parseText){
+        String[] splitPhrase = i.split("\\t");
+        String id = splitPhrase[1];
+        String upvotes = splitPhrase[2];
+        String downvotes = splitPhrase[3];
+        String comments = splitPhrase[4];
+        int totalVal = Integer.parseInt(upvotes) + Integer.parseInt(downvotes) + Integer.parseInt(comments);
+        IntWritable insertVal = new IntWritable(totalVal);
+        word.set(id);
+        context.write(word,insertVal);
+        /**
+        while (itr.hasMoreTokens()) {
+          word.set(itr.nextToken());
+          context.write(word, one);
+        }
+        */
+      }
+    }
+  }
+
+  public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(AnalyzeRedditData.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
